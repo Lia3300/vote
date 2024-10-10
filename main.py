480b69dc9a40efca3e27c6630d5ae836bdd32a5c
Lia3300 <liabecker3331@gmail.com>
2024-10-10 08:46:23 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+print("Welcome to voter eligibility tester.")
+print()
+age = int(input("please enter your name:"))
+citizen = input("are you an legal citizen of the us? (y/n)")
+registered = input("are you registered to vote? (y/n)")
+Incarceration = input("are you currently Incarcerated? (y/n)")
+
+
+if age >= 18: 
+
+  eligible = True
+else:
+  eligible = False
+
+if citizen =="y/n":
+  eligible = True
+else:
+  eligible = False
+
+  registerd = "y/n"
+  eligeble = True
+else:
+  eligible = false
+
+if incarcerated ="y/n"
+eligble = True
+else:
+eligible = False
+  if eligible
+  print
